d2cb040a3bbb97d7070a3dedc3b92b62bef3503f
Pushkar <Pushkar7397@gmail.com>
2021-09-03 10:58:52 +0530

    first commit

---
+++ operations.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main() {
+    int first,second,add,sub,mul,div,mod;
+    printf("Enter two numbers:\n");
+    scanf("%d%d",&first,&second);
+    add=first + second;
+    sub=first - second;
+    mul=first * second;
+    div=first / second;
+    mod=first % second;
+    printf("Addition = %d\nSubtraction = %d\nMultiplication = %d\nDivision = %d\nModulo = %d\n",add,sub,mul,div,mod);
+    
+    
+    
+    return 0;
+}
