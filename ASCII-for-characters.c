0dc0e0920f03957c9e9b03872ec3cb377f7915a3
Pushkar <Pushkar7397@gmail.com>
2021-09-10 11:40:12 +0530

    ASCII values for characters

---
+++ ASCII-for-characters.c
@@ -0,0 +1,13 @@
+//
+// Created by Pushkar on 10-09-2021.
+//
+#include <stdio.h>
+
+int main(){
+    char c;
+    printf("Enter character:");
+    scanf("%c",&c);
+    printf("ASCII value of character is %d",c);
+
+    return 0;
+}
